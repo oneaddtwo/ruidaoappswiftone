a2e634aad27c69c13a6e854a457547acad5a63f4
13110100622 <a13110100622@1111111.local>
2016-04-01 09:11:33 +0800

    睿道新建文件

---
+++ ruidaoappswift/ruidaoappswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ruidaoappswift
+//
+//  Created by 13110100622 on 16/4/1.
+//  Copyright © 2016年 王瑞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
